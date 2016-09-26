8d70294812793702715cbff71a46b1e5c130bf85
Arthur Pereira <arthurpolima@gmail.com>
2016-09-26 09:22:16 -0300

    Primeiro commit

---
+++ Xadrez boladao/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xadrez_boladao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite um valor inteiro: ");
+
+            int x = int.Parse(Console.ReadLine());
+            int y = 2 * x;
+            Console.WriteLine("DOBRO = " + y);
+            Console.ReadLine();
+
+
+        }
+    }
+}
